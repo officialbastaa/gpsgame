b2ad2f2fe2e0027212f17eb736cf5427032386e2
markusmeier345 <64257010+markusmeier345@users.noreply.github.com>
2020-12-11 13:07:26 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -80,10 +80,10 @@ function initMap() {
       iconImage: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
       },
       // Marker Mühle Tipp 3
-       {
-       coords:{lat: 53.07851, lng: 8.80457},
-       iconImage: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
-       }
+       //{
+       //coords:{lat: 53.07851, lng: 8.80457},
+       //iconImage: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+       //}
         
   ];
 
@@ -108,4 +108,39 @@ function initMap() {
       marker.setIcon(props.iconImage);
     }
   }
+
+  const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "sandstone rock formation in the southern part of the " +
+    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    "south west of the nearest large town, Alice Springs; 450&#160;km " +
+    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+    "Aboriginal people of the area. It has many springs, waterholes, " +
+    "rock caves and ancient paintings. Uluru is listed as a World " +
+    "Heritage Site.</p>" +
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+    "(last visited June 22, 2009).</p>" +
+    "</div>" +
+    "</div>";
+
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+
+  const marker = new google.maps.Marker({
+    position: {lat: 53.07851, lng: 8.80457},
+    map,
+    title: "Marker Mühle Tipp 3",
+  });
+  marker.addListener("click", () => {
+    infowindow.open(map, marker);
+  });
 }
